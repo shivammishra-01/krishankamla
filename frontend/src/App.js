c3dad494dc9289acdb55c68961409afb6415780f
Shivam Kumar Mishra <ershivamkumarmishra@gmail.com>
2025-08-04 19:40:06 +0530

    Your update message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,31 +3,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import AnimatedCarousel from './components/Pages/AnimatedCarousel';
+import Home from './components/Pages/Home';
+import Contact from './components/Pages/Contact'
 import './App.css';
-
-// Placeholder pages (add your actual pages here)
-// import WhoWeAre from './components/Pages/WhoWeAre';
-// import Service1 from './components/Pages/Service1';
-// import Service2 from './components/Pages/Service2';
-// import Careers from './components/Pages/Careers';
-// import Participate from './components/Pages/Participate';
-// import Contact from './components/Pages/Contact';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        
         <Routes>
-          <Route path="/" element={<AnimatedCarousel />} />
+          <Route path="/" element={<Home />} />
+          {/* Baaki routes agar chaaho to uncomment karke add karo */}
           {/* <Route path="/who" element={<WhoWeAre />} />
           <Route path="/service1" element={<Service1 />} />
           <Route path="/service2" element={<Service2 />} />
           <Route path="/careers" element={<Careers />} />
           <Route path="/participate" element={<Participate />} />
-          <Route path="/contact" element={<Contact />} /> */}
+          */}
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </div>
